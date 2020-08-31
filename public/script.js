ee1f14c23cf91bed8c264fd543547a13b8534ba6
Tarun Chinthakindi <46301788+tarunsunny3@users.noreply.github.com>
2020-09-01 00:31:46 +0530

    Created and able to make post and get requests

--- public/script.js
+++ public/script.js
@@ -2,15 +2,20 @@ const player = document.getElementById("player");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 const captureButton = document.getElementById("capture");
-
+const btn = document.getElementById("axios");
 const constraints = {
     video: true,
 };
 
-captureButton.addEventListener("click", () => {
+captureButton.addEventListener("click", async () => {
     context.drawImage(player, 0, 0, canvas.width, canvas.height);
     // console.log(canvas.toDataURL("image/png"));
-    console.log(context.getImageData(0, 0, canvas.width, canvas.height));
+    const imagedata = context.getImageData(0, 0, canvas.width, canvas.height);
+    console.log(imagedata);
+    tf.browser.fromPixels(imagedata).print();
+    const res = await axios.post('/authenticate', { imageData: imagedata });
+    console.log(res);
+    // console.log(context.getImageData(0, 0, canvas.width, canvas.height));
     // Stop all video streams.
     player.srcObject.getVideoTracks().forEach((track) => track.stop());
 });
@@ -20,4 +25,9 @@ navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
 
 
     player.srcObject = stream;
+});
+
+btn.addEventListener('click', () => {
+    console.log("button clicked");
+
 });
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
-
+app.use(bodyParser.json({ extended: true, limit: "50mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 app.get('/', (req, res) => {
     res.sendfile('index.html');
 })
+app.post('/authenticate', (req, res) => {
+    res.send("Authenticating post  route");
+})
+app.get('/authenticate', (req, res) => {
 
+    res.send("Authenticating route");
+})
 app.listen(5080, (req, res) => {
     console.log("Server started onn port 5000");
 });
