ff48c5a2a3053be2f0344fce34341f20f774a692
Rohan Reddy <43641071+rohanreddych@users.noreply.github.com>
2020-09-01 14:29:45 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,37 @@
+const player = document.getElementById("player");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const captureButton = document.getElementById("capture");
+const btn = document.getElementById("axios");
+const constraints = {
+	video: true,
+};
+
+captureButton.addEventListener("click", async () => {
+	context.drawImage(player, 0, 0, canvas.width, canvas.height);
+	// console.log(canvas.toDataURL("image/png"));
+	var dataURL = canvas.toDataURL("image/png");
+	const imagedata = context.getImageData(0, 0, canvas.width, canvas.height);
+	//console.log(imagedata);
+	tf.browser.fromPixels(imagedata).print();
+	const res = await axios.post("/authenticate", {
+		imageData: imagedata,
+		dataURL: dataURL,
+		width: canvas.width,
+		height: canvas.height,
+	});
+	//console.log(res);
+	// console.log(context.getImageData(0, 0, canvas.width, canvas.height));
+	// Stop all video streams.
+	player.srcObject.getVideoTracks().forEach((track) => track.stop());
+});
+
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+	// Attach the video stream to the video element and autoplay.
+
+	player.srcObject = stream;
+});
+
+btn.addEventListener("click", () => {
+	console.log("button clicked");
+});
