c188087494d5e997234919601393ae942d3f52cd
Tarun Chinthakindi <46301788+tarunsunny3@users.noreply.github.com>
2020-09-04 19:03:00 +0530

    Server code login and register routes

--- public/script.js
+++ public/script.js
@@ -4,30 +4,83 @@ const context = canvas.getContext("2d");
 const captureButton = document.getElementById("capture");
 const btn = document.getElementById("axios");
 const constraints = {
-    video: true,
+	video: true,
 };
+async function test() {
+	console.log("Entered");
+	const model = await blazeface.load();
+	//console.log(body.dataURI);
+	// var img = func(body.dataURL);
+	// console.log(img);
+	// console.log(body.imageData.length);
+	// console.log("From Test" + array.subarray(0, 10));
+	const predictions = await model.estimateFaces(
+		document.getElementById("photo"),
+		false
+	);
+	if (predictions.length > 0) {
+		/*
+		`predictions` is an array of objects describing each detected face, for example:
+	
+		[
+		  {
+			topLeft: [232.28, 145.26],
+			bottomRight: [449.75, 308.36],
+			probability: [0.998],
+			landmarks: [
+			  [295.13, 177.64], // right eye
+			  [382.32, 175.56], // left eye
+			  [341.18, 205.03], // nose
+			  [345.12, 250.61], // mouth
+			  [252.76, 211.37], // right ear
+			  [431.20, 204.93] // left ear
+			]
+		  }
+		]
+		*/
 
+		for (let i = 0; i < predictions.length; i++) {
+			const start = predictions[i].topLeft;
+			const end = predictions[i].bottomRight;
+			const size = [end[0] - start[0], end[1] - start[1]];
+			console.log(predictions[i]);
+			// Render a rectangle over each detected face.
+			context.drawImage(photo, 0, 0, 400, 400);
+			// console.log(context);
+			context.fillStyle = "red";
+			context.globalAlpha = 0.5;
+			context.fillRect(start[0], start[1], size[0], size[1]);
+		}
+	}
+	// console.log(predictions);
+}
 captureButton.addEventListener("click", async () => {
-    context.drawImage(player, 0, 0, canvas.width, canvas.height);
-    // console.log(canvas.toDataURL("image/png"));
-    const imagedata = context.getImageData(0, 0, canvas.width, canvas.height);
-    console.log(imagedata);
-    tf.browser.fromPixels(imagedata).print();
-    const res = await axios.post('/authenticate', { imageData: imagedata });
-    console.log(res);
-    // console.log(context.getImageData(0, 0, canvas.width, canvas.height));
-    // Stop all video streams.
-    player.srcObject.getVideoTracks().forEach((track) => track.stop());
+	context.drawImage(player, 0, 0, canvas.width, canvas.height);
+	// console.log(canvas.toDataURL("image/png"));
+	var dataURL = canvas.toDataURL("image/png");
+	const imagedata = context.getImageData(0, 0, canvas.width, canvas.height);
+	//console.log(imagedata);
+	// tf.browser.fromPixels(imagedata).print();
+	// console.log(imagedata.data);
+	// const res = await axios.post("/authenticate", {
+	// 	imageData: imagedata.data,
+	// 	dataURL: dataURL,
+	// 	width: canvas.width,
+	// 	height: canvas.height,
+	// });
+	//console.log(res);
+	// console.log(context.getImageData(0, 0, canvas.width, canvas.height));
+	// Stop all video streams.
+
+	player.srcObject.getVideoTracks().forEach((track) => track.stop());
 });
 
 navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-    // Attach the video stream to the video element and autoplay.
+	// Attach the video stream to the video element and autoplay.
 
-
-    player.srcObject = stream;
+	player.srcObject = stream;
 });
-
-btn.addEventListener('click', () => {
-    console.log("button clicked");
-
+test();
+btn.addEventListener("click", () => {
+	console.log("button clicked");
 });
--- server.js
+++ server.js
@@ -1,20 +1,100 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+// require('@tensorflow/tfjs-node');
+const bodyParser = require("body-parser");
 const app = express();
+const mysql = require('mysql');
+
+const fs = require("fs");
+const blazeface = require("@tensorflow-models/blazeface");
+var atob = require("atob");
+var con = mysql.createConnection({
+	host: "database-personalcards.csijlxmxisod.us-east-1.rds.amazonaws.com",
+	user: "admin",
+	password: "personal",
+	port: "3306",
+	database: "personalcards"
+});
+
+function func(dataURI) {
+	//const model = await blazeface.load();
+	//var Img = new ImageData(img);
+	//console.log(Img);
+
+	//console.log(typeof img);
+	//const predictions = await model.estimateFaces(img, false);
+	//console.log(predictions);
+	var BASE64_MARKER = ";base64,";
+
+	var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+	var base64 = dataURI.substring(base64Index);
+	var raw = atob(base64);
+	var rawLength = raw.length;
+	var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+	for (i = 0; i < rawLength; i++) {
+		array[i] = raw.charCodeAt(i);
+	}
+
+	return array;
+}
+
+async function test(array, width, height) {
+	const model = await blazeface.load();
+	//console.log(body.dataURI);
+	// var img = func(body.dataURL);
+	// console.log(img);
+	// console.log(body.imageData.length);
+	console.log("From Test" + array.subarray(0, 10));
+	const predictions = await model.estimateFaces(
+		{ data: array, width, height },
+		true
+	);
+	console.log(predictions);
+}
 
 app.use(bodyParser.json({ extended: true, limit: "50mb" }));
 app.use(express.static(path.join(__dirname, "public")));
-app.get('/', (req, res) => {
-    res.sendfile('index.html');
+app.get("/", (req, res) => {
+	res.sendfile("index.html");
+});
+app.post('/hello', (req, res) => {
+	res.send("Hello route");
 })
-app.post('/authenticate', (req, res) => {
-    res.send("Authenticating post  route");
-})
-app.get('/authenticate', (req, res) => {
+app.post('/login', (req, res) => {
+	const userName = req.body.username;
+	const pass = req.body.password;
+	console.log(userName);
+	console.log(pass);
+	var print = `SELECT * FROM Users WHERE username = ?`;
+	console.log(print);
+	con.connect((err) => {
+		if (err) throw err;
+		console.log("Connected!");
+		// var sql = "INSERT INTO Users (username, password) VALUES ('Sunny', 'hai'),('Vinny', 'Enti')";
+		con.query(print, [userName], (err, result, fields) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(result);
+			}
+			// console.log("2 record inserted");
+		});
 
-    res.send("Authenticating route");
+	});
+	res.send("Login Route");
 })
-app.listen(5080, (req, res) => {
-    console.log("Server started onn port 5000");
+app.post("/authenticate", async (req, res) => {
+	res.send("Authenticating post  route");
+	//console.log(req.body.dataURL)
+	const body = await (req.body);
+	const uintArray = Uint32Array.from(Object.values(body.imageData));
+	// console.log(uintArray.subarray(0, 10));
+	test(uintArray, body.width, body.height);
+});
+app.get("/authenticate", (req, res) => {
+	res.send("Authenticating route");
+});
+app.listen(5000, (req, res) => {
+	console.log("Server started onn port 5000");
 });
