d09b03c1780cfb514d1d0bdd154f37b286c89d26
Nitin Sai <musicssns@gmail.com>
2021-01-03 16:30:37 +0530

    cleaned

--- Google Authenticator/index.js
+++ Google Authenticator/index.js
@@ -1,13 +1,35 @@
 const speakeasy =  require('speakeasy')
-const qrcode = require('qrcode') 
+const bcrypt = require('bcrypt');
+const User = require("./models/User");
+async function gAuthenticate(username, password){
+    const hash = bcrypt.hashSync(password, 10);
+    // const decrypt = bcrypt.compareSync(password, hash);
+    var secret = speakeasy.generateSecret({
+        name: username
+    })
 
-var secret = speakeasy.generateSecret({
-    name: "Chinnu"
-})
+    const user = new User({
+        username,
+        password: hash,
+        secret:{
+            ascii: secret.ascii,
+            imageURL: secret.otpauth_url
+        }
+    });
+    try {
+        const savedUser = await user.save();
+        console.log("Successfully written to DB");
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-console.log(secret)
-
-qrcode.toDataURL(secret.otpauth_url, function(err, data){
-    if (err) throw err;
-    console.log(data)
-})
+function gValidate(otp, ascii){
+    var result = speakeasy.totp.verify({
+        secret: ascii,
+        encoding: 'ascii',
+        token: otp
+    });
+    return result;
+}
+module.exports = {gAuthenticate, gValidate};
--- qrcode1/validate.js
+++
@@ -1,9 +0,0 @@
-const speakeasy = require('speakeasy')
-
-var result = speakeasy.totp.verify({
-    secret: '^SwNBxc.:CT:({,0J[A*bl<KbRrfHoJL',
-    encoding: 'ascii',
-    token: '065685'
-})
-
-console.log(result)
