77e4b2077479b312d7b4c6a7107b99363e7f783a
Nitin Sai <musicssns@gmail.com>
2021-01-12 16:01:06 +0530

    added py code

---
+++ face-authentication/verify.py
@@ -0,0 +1,19 @@
+import face_recognition
+import os
+import cv2
+import numpy as np
+import csv
+from PIL import Image
+import sys
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+image = sys.argv[1]
+
+print("I am nitin")
+
+img = mpimg(image)
+plt.show()
+
+sys.stdout.flush()
+exit()
--- routes/face_auth.js
+++ routes/face_auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../db/schema');
 const { requireAuth } = require('../middlewares/authToken');
+const spawn = require("child_process").spawn;
 
 router.post('/face_sign_in', requireAuth, async(req,res)=>{
     try{
@@ -26,6 +27,16 @@ router.post('/face_sign_up', requireAuth, async(req,res)=>{
         const studentid = await req.decoded.studentid;
         const image64 = await req.body.image64;
         
+        function auth(image64) {
+            const pythonProcess = spawn("python", ["test.py", image64]);
+            pythonProcess.stdout.on("data", (data) => {
+                console.log(data.toString());
+                //if authenticated then redirect
+            });
+        }
+
+        await auth(image64);
+
         const userDetails = await User.findOne({studentid: studentid}).exec();
         userDetails.faceRecognitionImage.push(image64);
         console.log('Image is saved in database');
