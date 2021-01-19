c13db15466faee873c0b81921bf5fa497f121ce2
Yashi1011 <yasaswinipandu@gmail.com>
2021-01-19 17:38:39 +0530

    user dashboard css

--- website-frontend/src/Options.js
+++ website-frontend/src/Options.js
@@ -1,12 +1,14 @@
 import { Component} from 'react';
 import {Link} from 'react-router-dom';
+
+
 class Options extends Component {
   
   render() {  
     return (
-        <div>
-          <Link to="/passwordlogin">Login with password</Link>
-          <Link to="/faceLogin">Login with Face</Link>
+        <div className="center">
+          <div className="cards"><Link to="/passwordlogin">Login with password</Link></div>
+          <div className="cards"><Link to="/faceLogin">Login with Face</Link></div>
         </div>
       );
   }
