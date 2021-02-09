359c56e84ba22ed5369e76d6cfedf492f934577f
Nitin1901 <nitinsai.bommi@gmail.com>
2021-02-09 21:49:50 +0530

    worked on redirects

--- website-frontend/src/components/Options.js
+++ website-frontend/src/components/Options.js
@@ -1,49 +1,72 @@
-import { Component} from 'react';
-import {Link} from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
 // import Col from 'react-bootstrap/Col';
-import face from '../images/face.png';
-import passwordimage from '../images/password.png';
-
+import face from "../images/face.png";
+import passwordimage from "../images/password.png";
+import { getUserRole } from "../utils.js";
 
 class Options extends Component {
-
-  render() {
-    return (
-        <div>
-          <div className="heading">
-            <h1>Select Mode of Login</h1>
-          </div>
-          <table className="table-center">
-            <tbody>
-            <tr>
-              <td>
-                <Link className="text-link d-inline p-2" to="/passwordlogin">
-                  <Card style={{ width: '18rem' }} className="text-center">
-                    <Card.Img variant="top" src= {passwordimage} />
-                    <Card.Body>
-                      <Card.Title>Login with password</Card.Title>
-                    </Card.Body>
-                  </Card>
-                </Link>
-              </td>
-              <td>
-                <Link className="text-link d-inline p-2" to="/faceLogin">
-                  <Card style={{ width: '18rem' }} className="text-center">
-                    <Card.Img variant="top" src={face} />
-                    <Card.Body>
-                      <Card.Title>Login with Face</Card.Title>
-                    </Card.Body>
-                  </Card>
-                </Link>
-              </td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
-      );
+  constructor(props) {
+    super(props);
+    this.state = {
+      notLoggedIn: false,
+    };
   }
-
+  async componentDidMount() {
+    const tokenRole = await getUserRole();
+    const role = await getUserRole();
+    if (role === "Student") {
+      window.location.replace("/studentDashboard");
+    } else if (role === "Professor") {
+      window.location.replace("/professorDashboard");
+    } else {
+      this.setState({ notLoggedIn: true });
+    }
+  }
+  render() {
+    let notLoggedIn = this.state.notLoggedIn;
+    return (
+      <div>
+        {notLoggedIn ? (
+          <div>
+            <div className="heading">
+              <h1>Select Mode of Login</h1>
+            </div>
+            <table className="table-center">
+              <tbody>
+                <tr>
+                  <td>
+                    <Link
+                      className="text-link d-inline p-2"
+                      to="/passwordlogin"
+                    >
+                      <Card style={{ width: "18rem" }} className="text-center">
+                        <Card.Img variant="top" src={passwordimage} />
+                        <Card.Body>
+                          <Card.Title>Login with password</Card.Title>
+                        </Card.Body>
+                      </Card>
+                    </Link>
+                  </td>
+                  <td>
+                    <Link className="text-link d-inline p-2" to="/faceLogin">
+                      <Card style={{ width: "18rem" }} className="text-center">
+                        <Card.Img variant="top" src={face} />
+                        <Card.Body>
+                          <Card.Title>Login with Face</Card.Title>
+                        </Card.Body>
+                      </Card>
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default Options;
--- website-frontend/src/components/PasswordLogin.js
+++ website-frontend/src/components/PasswordLogin.js
@@ -1,57 +1,67 @@
-import { Component} from 'react';
-import axios from 'axios';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import {getUserRole, logout} from '../utils.js';
+import { Component } from "react";
+import axios from "axios";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { getUserRole, logout } from "../utils.js";
 
 class PasswordLogin extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       userid: "",
       password: "",
       counter: 0,
+      notLoggedIn: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  async componentDidMount() {
+    const tokenRole = await getUserRole();
+    const role = await getUserRole();
+    if (role === "Student") {
+      window.location.replace("/studentDashboard");
+    } else if (role === "Professor") {
+      window.location.replace("/professorDashboard");
+    } else {
+      this.setState({ notLoggedIn: true });
     }
-    this.handleChange=this.handleChange.bind(this);
-    this.handleSubmit=this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
-    this.setState({[e.target.name]: e.target.value})
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  async handleSubmit(e){
+  async handleSubmit(e) {
     e.preventDefault();
-    if(this.state.counter < 3){
-
-
-      if(this.state.password!==""){
-        const res = await axios.post("http://localhost:8080/api/passwordlogin/", {password: this.state.password});
-        if(res.data.success){
+    if (this.state.counter < 3) {
+      if (this.state.password !== "") {
+        const res = await axios.post(
+          "http://localhost:8080/api/passwordlogin/",
+          { password: this.state.password }
+        );
+        if (res.data.success) {
           //If token is not found in cookie and server throws an error
           //Then redirect to checkid page
-          if(res.data.error === "token"){
-            window.location.replace('/');
+          if (res.data.error === "token") {
+            window.location.replace("/");
           }
-          if(res.data.result.role === 'Student'){
-
-                window.location.replace("/studentDashboard");
-
-          }else{
+          if (res.data.result.role === "Student") {
+            window.location.replace("/studentDashboard");
+          } else {
             // this.props.handleLogin("professor");
             window.location.replace("/professorDashboard");
           }
-
-        }else{
+        } else {
           alert(res.data.message);
           this.setState({ counter: this.state.counter + 1 });
         }
-      }else{
-        alert("Password must not be blank")
+      } else {
+        alert("Password must not be blank");
       }
-    }else{
+    } else {
       alert("Incorrect password entered too many times, please enter ID again");
-      if(await getUserRole()){
+      if (await getUserRole()) {
         logout();
         window.location.replace("/");
       }
@@ -59,23 +69,32 @@ class PasswordLogin extends Component {
   }
 
   render() {
+    let notLoggedIn = this.state.notLoggedIn;
     return (
-        <div>
+      <div>
+        {notLoggedIn ? (
           <div className="form-form">
             <Form onSubmit={this.handleSubmit}>
               <Form.Group controlId="password">
-                <Form.Control type="password" className="item" name="password"  placeholder="Enter Password" onChange={this.handleChange} />
+                <Form.Control
+                  type="password"
+                  className="item"
+                  name="password"
+                  placeholder="Enter Password"
+                  onChange={this.handleChange}
+                />
               </Form.Group>
               <Form.Group controlId="submitbutton">
-                  <Button type="submit" className="create-account">Login</Button>
+                <Button type="submit" className="create-account">
+                  Login
+                </Button>
               </Form.Group>
             </Form>
           </div>
-
-        </div>
-      );
+        ) : null}
+      </div>
+    );
   }
-
 }
 
 export default PasswordLogin;
